7d041e7ae75e2adc372409188cb38c0e4df886ea
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-01-03 20:51:24 +0100

    bugfix: default preferencen

--- src/de/dmarcini/submatix/android4/gui/areaListActivity.java
+++ src/de/dmarcini/submatix/android4/gui/areaListActivity.java
@@ -6,12 +6,10 @@
  */
 package de.dmarcini.submatix.android4.gui;
 
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
-
 import de.dmarcini.submatix.android4.BuildConfig;
 import de.dmarcini.submatix.android4.R;
 import de.dmarcini.submatix.android4.content.ContentSwitcher;
@@ -25,7 +23,9 @@ import de.dmarcini.submatix.android4.utils.ProjectConst;
  */
 public class areaListActivity extends FragmentCommonActivity
 {
-  private static final String TAG = areaListActivity.class.getSimpleName();
+  private static final String TAG         = areaListActivity.class.getSimpleName();
+  private static final String FIRSTTIME   = "keyFirstTimeInitiated";
+  private static final String PREFVERSION = "keyPreferencesVersion";
 
   /**
    * Erzeuge die Menüeinträge Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.gui
@@ -34,23 +34,23 @@ public class areaListActivity extends FragmentCommonActivity
    */
   private void initStaticContenSwitcher()
   {
-    Log.v(TAG, "initStaticContent...");
+    Log.v( TAG, "initStaticContent..." );
     // zuerst aufräumen
     ContentSwitcher.clearItems();
     //
     // irgendeine Kennung muss der String bekommen, also gibts halt die String-ID
     //
-    ContentSwitcher.addItem(new ProgItem(R.string.progitem_connect, R.drawable.bluetooth_icon_color, getString(R.string.progitem_connect)));
-    ContentSwitcher.addItem(new ProgItem(R.string.progitem_config, R.drawable.toolboxwhite, getString(R.string.progitem_config)));
-    ContentSwitcher.addItem(new ProgItem(R.string.progitem_gaslist, R.drawable.pinion, getString(R.string.progitem_gaslist)));
-    ContentSwitcher.addItem(new ProgItem(R.string.progitem_logging, R.drawable.logging, getString(R.string.progitem_logging)));
-    ContentSwitcher.addItem(new ProgItem(R.string.progitem_progpref, R.drawable.toolboxred, getString(R.string.progitem_progpref)));
-    ContentSwitcher.addItem(new ProgItem(R.string.progitem_exit, R.drawable.shutoff, getString(R.string.progitem_exit)));
-    if (BuildConfig.DEBUG)
+    ContentSwitcher.addItem( new ProgItem( R.string.progitem_connect, R.drawable.bluetooth_icon_color, getString( R.string.progitem_connect ) ) );
+    ContentSwitcher.addItem( new ProgItem( R.string.progitem_config, R.drawable.toolboxwhite, getString( R.string.progitem_config ) ) );
+    ContentSwitcher.addItem( new ProgItem( R.string.progitem_gaslist, R.drawable.pinion, getString( R.string.progitem_gaslist ) ) );
+    ContentSwitcher.addItem( new ProgItem( R.string.progitem_logging, R.drawable.logging, getString( R.string.progitem_logging ) ) );
+    ContentSwitcher.addItem( new ProgItem( R.string.progitem_progpref, R.drawable.toolboxred, getString( R.string.progitem_progpref ) ) );
+    ContentSwitcher.addItem( new ProgItem( R.string.progitem_exit, R.drawable.shutoff, getString( R.string.progitem_exit ) ) );
+    if( BuildConfig.DEBUG )
     {
-      ContentSwitcher.addItem(new ProgItem(R.string.progitem_null, R.drawable.placeholder, getString(R.string.progitem_null)));
-      ContentSwitcher.addItem(new ProgItem(R.string.progitem_set_defaults, R.drawable.radiation, getString(R.string.progitem_set_defaults)));
-      ContentSwitcher.addItem(new ProgItem(R.string.progitem_log_propertys, R.drawable.radiation, getString(R.string.progitem_log_propertys)));
+      ContentSwitcher.addItem( new ProgItem( R.string.progitem_null, R.drawable.placeholder, getString( R.string.progitem_null ) ) );
+      ContentSwitcher.addItem( new ProgItem( R.string.progitem_set_defaults, R.drawable.radiation, getString( R.string.progitem_set_defaults ) ) );
+      ContentSwitcher.addItem( new ProgItem( R.string.progitem_log_propertys, R.drawable.radiation, getString( R.string.progitem_log_propertys ) ) );
     }
   }
 
@@ -62,18 +62,18 @@ public class areaListActivity extends FragmentCommonActivity
    * @param savedInstanceState
    */
   @Override
-  public void onCreate(Bundle savedInstanceState)
+  public void onCreate( Bundle savedInstanceState )
   {
-    super.onCreate(savedInstanceState);
-    Log.v(TAG, "onCreate...");
+    super.onCreate( savedInstanceState );
+    Log.v( TAG, "onCreate..." );
     //
     // wurden jemals Preferences gesetzt?
     //
-    SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences(this);
-    if (!sPref.contains("firstTimeInitiated"))
+    SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences( this );
+    if( !sPref.contains( FIRSTTIME ) )
     {
-      Log.w(TAG, "onCreate: make first time preferences...");
-      if (!sPref.getBoolean("firstTimeInitiated", false))
+      Log.w( TAG, "onCreate: not found firsttime key == make first time preferences..." );
+      if( !sPref.getBoolean( FIRSTTIME, false ) )
       {
         setDefaultPreferences();
       }
@@ -81,73 +81,59 @@ public class areaListActivity extends FragmentCommonActivity
     //
     // sind die Preferenzen in der richtigen version?
     //
-    if (sPref.contains("PREF_VERSION"))
+    if( sPref.contains( PREFVERSION ) )
     {
-      if (ProjectConst.PREF_VERSION != sPref.getInt("PREF_VERSION", 0))
+      if( ProjectConst.PREF_VERSION != sPref.getInt( PREFVERSION, 0 ) )
       {
+        Log.w( TAG, "onCreate: pref version to old == make first time preferences..." );
         setDefaultPreferences();
       }
     }
     else
     {
+      Log.w( TAG, "onCreate: pref version not found == make first time preferences..." );
       setDefaultPreferences();
-    }
-    //
-    // Das gewünschte Thema setzen
-    //
-    if (sPref.contains("keyAppTheme"))
-    {
-      int whishedTheme = sPref.getInt("keyAppTheme", R.style.AppDarkTheme);
-      switch (whishedTheme)
-      {
-        case R.style.AppBlueTheme:
-          setTheme(R.style.AppBlueTheme);
-          break;
-        case R.style.AppDarkTheme:
-        default:
-          setTheme(R.style.AppDarkTheme);
-      }
     }
     //
     // guck mal. ob das ein grosses Display ist,
     // dann ist da nämlich auch der Detailcontainer vorhanden
     //
-    if (findViewById(R.id.area_detail_container) != null)
+    if( findViewById( R.id.area_detail_container ) != null )
     {
-      Log.v(TAG, "onCreate: twoPane-mode");
+      Log.v( TAG, "onCreate: twoPane-mode" );
       // Der Detailcontainer ist nur vorhanden, wenn die App
       // (durch das System) festgestellt hat, dass es auf einem
       // Gerät mirt grossem Display (res/values-large and
       // res/values-sw600dp) läuft.
       mTwoPane = true;
       // Im twoPane Modus soll der aktivierte Eintrag immer gekennzeichnet sein!
-      Log.v(TAG, "onCreate: set \"activate on item click\"...");
-      ((areaListFragment) getFragmentManager().findFragmentById(R.id.area_list)).setActivateOnItemClick(true);
+      Log.v( TAG, "onCreate: set \"activate on item click\"..." );
+      ( ( areaListFragment )getFragmentManager().findFragmentById( R.id.area_list ) ).setActivateOnItemClick( true );
     }
     else
     {
-      Log.v(TAG, "onCreate: onePane-mode");
+      Log.v( TAG, "onCreate: onePane-mode" );
     }
     //
     // die Programmlisteneinträge initialisieren
     //
-    Log.v(TAG, "onCreate: initStaticContentSwitcher()...");
+    Log.v( TAG, "onCreate: initStaticContentSwitcher()..." );
     initStaticContenSwitcher();
-    Log.v(TAG, "onCreate: initStaticContentSwitcher()...OK");
+    Log.v( TAG, "onCreate: initStaticContentSwitcher()...OK" );
   }
 
   @Override
   public void onPause()
   {
     super.onPause();
-    Log.v(TAG, "onPause...");
+    Log.v( TAG, "onPause..." );
   }
 
   @Override
   public void onResume()
   {
     super.onResume();
-    Log.v(TAG, "onResume...");
+    Log.v( TAG, "onResume..." );
   }
 
   /**
@@ -157,17 +143,34 @@ public class areaListActivity extends FragmentCommonActivity
    */
   public void setDefaultPreferences()
   {
-    Log.i(TAG, "setDefaultPreferences: make default preferences...");
-    PreferenceManager.getDefaultSharedPreferences(this);
-    PreferenceManager.setDefaultValues(this, R.xml.config_spx42_preference_individual, true);
-    PreferenceManager.setDefaultValues(this, R.xml.config_program_preference, true);
-    SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences(this);
+    String gasKeyTemplate = getResources().getString( R.string.conf_gaslist_gas_key_template );
+    String gasListDefault = getResources().getString( R.string.conf_gaslist_default );
+    Log.d( TAG, "setDefaultPreferences: make default preferences..." );
+    PreferenceManager.setDefaultValues( this, R.xml.config_spx42_preference_individual, true );
+    PreferenceManager.setDefaultValues( this, R.xml.config_program_preference, true );
     //
     // workarround um defaults zu setzen
     //
-    sPref.edit().putBoolean("firstTimeInitiated", true);
-    sPref.edit().commit();
-    sPref.edit().putInt("PREF_VERSION", ProjectConst.PREF_VERSION);
-    sPref.edit().commit();
+    SharedPreferences sPref = PreferenceManager.getDefaultSharedPreferences( this );
+    SharedPreferences.Editor editor = sPref.edit();
+    editor.remove( FIRSTTIME );
+    editor.putBoolean( FIRSTTIME, true );
+    editor.remove( PREFVERSION );
+    editor.putInt( PREFVERSION, ProjectConst.PREF_VERSION );
+    //
+    // Gaslistenpresets eintragen
+    //
+    for( int i = 1; i < 9; i++ )
+    {
+      editor.putString( String.format( gasKeyTemplate, i ), gasListDefault );
+    }
+    if( editor.commit() )
+    {
+      Log.d( TAG, "setDefaultPreferences: wrote preferences to storeage." );
+    }
+    else
+    {
+      Log.e( TAG, "setDefaultPreferences: CAN'T wrote preferences to storeage." );
+    }
   }
 }
