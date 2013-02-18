d049a7fa76503b69b09285453ede43cdd0f16bde
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-02-18 23:13:18 +0100

    Exceptiontyp für nicht implementierte Features

---
+++ src/de/dmarcini/submatix/android4/utils/NotImplementedFunctionException.java
@@ -0,0 +1,14 @@
+package de.dmarcini.submatix.android4.utils;
+
+public class NotImplementedFunctionException extends Exception
+{
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 3324521021204996876L;
+
+  public NotImplementedFunctionException( String msg )
+  {
+    super( msg );
+  }
+}
