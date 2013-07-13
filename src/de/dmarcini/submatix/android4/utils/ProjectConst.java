4c9156f91cd48d68c01ab6cffd0dac81ba928f85
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-07-13 13:35:47 +0200

    Messages für DECO zugefügt

--- src/de/dmarcini/submatix/android4/utils/ProjectConst.java
+++ src/de/dmarcini/submatix/android4/utils/ProjectConst.java
@@ -163,6 +163,8 @@ public final class ProjectConst
   public static final int    MESSAGE_SETPOINT_READ      = ActionEvent.RESERVED_ID_MAX + 13;
   public static final int    MESSAGE_SETPOINT_ACK       = ActionEvent.RESERVED_ID_MAX + 14;
   public static final int    MESSAGE_LICENSE_STATE_READ = ActionEvent.RESERVED_ID_MAX + 15;
+  public static final int    MESSAGE_DECO_READ          = ActionEvent.RESERVED_ID_MAX + 16;
+  public static final int    MESSAGE_DECO_ACK           = ActionEvent.RESERVED_ID_MAX + 17;
   
   //
 //  public static final int    MESSAGE_STATE_CHANGE       = ActionEvent.RESERVED_ID_MAX + 2;
@@ -170,7 +172,6 @@ public final class ProjectConst
 //  public static final int    MESSAGE_WRITE              = ActionEvent.RESERVED_ID_MAX + 4;
 //  public static final int    MESSAGE_DEVICE_NAME        = ActionEvent.RESERVED_ID_MAX + 5;
 //  public static final int    MESSAGE_TOAST              = ActionEvent.RESERVED_ID_MAX + 6;
-//  public static final int    MESSAGE_DECO_READ          = ActionEvent.RESERVED_ID_MAX + 8;
 //  public static final int    MESSAGE_DISPLAY_READ       = ActionEvent.RESERVED_ID_MAX + 10;
 //  public static final int    MESSAGE_INDIVID_READ       = ActionEvent.RESERVED_ID_MAX + 11;
 //  public static final int    MESSAGE_GAS_READ           = ActionEvent.RESERVED_ID_MAX + 12;
