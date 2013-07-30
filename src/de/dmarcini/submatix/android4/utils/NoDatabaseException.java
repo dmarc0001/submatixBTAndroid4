6028251930bde0d1f799a5bf889f2cc2e5d90d43
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-07-30 22:02:15 +0200

    Eigen exception Kategorie eingebaut

---
+++ src/de/dmarcini/submatix/android4/utils/NoDatabaseException.java
@@ -0,0 +1,14 @@
+package de.dmarcini.submatix.android4.utils;
+
+public class NoDatabaseException extends Exception
+{
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 3324521021204996876L;
+
+  public NoDatabaseException( String msg )
+  {
+    super( msg );
+  }
+}
