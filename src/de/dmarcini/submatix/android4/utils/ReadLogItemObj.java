6d29cde4928f1362ea6866e7dc4497d477a8aec8
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-08-07 00:04:18 +0200

    Parameter erweitert

--- src/de/dmarcini/submatix/android4/utils/ReadLogItemObj.java
+++ src/de/dmarcini/submatix/android4/utils/ReadLogItemObj.java
@@ -4,15 +4,119 @@ public class ReadLogItemObj
 {
   public boolean isSaved;
   public String  itemName;
+  public String  itemNameOnSPX;
   public String  itemDetail;
+  public int     dbId;
+  public int     numberOnSPX;
+  public boolean isMarked = false;
 
+  /**
+   * 
+   * Konstruktor für einen Loglist Eintrag
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 06.08.2013
+   * @param _isSaved
+   * @param _itemName
+   * @param _itemDetail
+   */
   public ReadLogItemObj( final boolean _isSaved, final String _itemName, final String _itemDetail )
   {
     this.isSaved = _isSaved;
     this.itemName = _itemName;
+    this.itemNameOnSPX = _itemName;
     this.itemDetail = _itemDetail;
+    this.dbId = -1;
+    this.numberOnSPX = -1;
   }
 
+  /**
+   * 
+   * Konstruktor für einen Loglist Eintrag mit Datenbankid
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 06.08.2013
+   * @param _isSaved
+   * @param _itemName
+   * @param _itemDetail
+   * @param _dbid
+   */
+  public ReadLogItemObj( final boolean _isSaved, final String _itemName, final String _itemDetail, final int _dbid )
+  {
+    this.isSaved = _isSaved;
+    this.itemName = _itemName;
+    this.itemNameOnSPX = _itemName;
+    this.itemDetail = _itemDetail;
+    this.dbId = _dbid;
+    this.numberOnSPX = -1;
+  }
+
+  /**
+   * 
+   * Konstruktor für einen Loglist Eintrag mit Datenbankid und Numer auf SPX
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 06.08.2013
+   * @param _isSaved
+   * @param _itemName
+   * @param _itemDetail
+   * @param _dbid
+   * @param _numOnSpx
+   */
+  public ReadLogItemObj( final boolean _isSaved, final String _itemName, final String _itemDetail, final int _dbid, final int _numOnSpx )
+  {
+    this.isSaved = _isSaved;
+    this.itemName = _itemName;
+    this.itemNameOnSPX = _itemName;
+    this.itemDetail = _itemDetail;
+    this.dbId = _dbid;
+    this.numberOnSPX = _numOnSpx;
+  }
+
+  @SuppressWarnings( "unused" )
   private ReadLogItemObj()
   {}
+
+  /**
+   * 
+   * Setze merkiert oder nicht
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 06.08.2013
+   * @param position
+   * @param marked
+   */
+  public void setMarked( int position, boolean marked )
+  {
+    this.isMarked = marked;
+  }
+
+  /**
+   * 
+   * Gib zurück, ob markiert oder nicht
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 06.08.2013
+   * @param position
+   * @return
+   */
+  public boolean getMarked( int position )
+  {
+    return( this.isMarked );
+  }
 }
