e8fe9a6e8d72fca52f354e8a732caa213732b1f7
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-08-07 00:01:57 +0200

    automatisch erzeugt

--- src/de/dmarcini/submatix/android4/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/utils/BuildVersion.java
@@ -15,8 +15,8 @@ import java.util.Date;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 2379L;
-  private static final long buildDate = 1375815298065L;
+  private static final long buildNumber = 2436L;
+  private static final long buildDate = 1375826417868L;
   private static final String buildNumberString = String.format( "%d", buildNumber );
   private static final String buildDateString   = new Date( buildDate ).toString();
 
