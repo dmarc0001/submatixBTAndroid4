21c4a8f0570e04d408733c418812ca7c0b88538f
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-08-13 22:49:05 +0200

    Funktion zum Sichern eines Tauchgangs zugefügt

--- src/de/dmarcini/submatix/android4/utils/SPX42LogManager.java
+++ src/de/dmarcini/submatix/android4/utils/SPX42LogManager.java
@@ -1,5 +1,6 @@
 package de.dmarcini.submatix.android4.utils;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
@@ -51,5 +52,37 @@ public class SPX42LogManager extends SPX42AliasManager
     cu.close();
     if( BuildConfig.DEBUG ) Log.i( TAG, "isLogInDatabase... datasets is <" + count + ">" );
     return( ( count == 0 ) ? false : true );
+  }
+
+  /**
+   * 
+   * Den Tauchgang nach erfolgter Erstellung der XML-Datei in die DB eintragen
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 13.08.2013
+   * @param diveHeader
+   */
+  public boolean saveDive( SPX42DiveHeadData diveHeader )
+  {
+    ContentValues values;
+    //
+    // nein, das existiert noch nicht
+    //
+    values = new ContentValues();
+    values.put( ProjectConst.H_FILEONMOBILE, diveHeader.xmlFile.getAbsolutePath() );
+    values.put( ProjectConst.H_DIVENUMBERONSPX, diveHeader.diveNumberOnSPX );
+    values.put( ProjectConst.H_DEVICESERIAL, diveHeader.deviceSerialNumber );
+    values.put( ProjectConst.H_STARTTIME, diveHeader.startTime ); // TODO: prüfe, ob das so hinkommt
+    values.put( ProjectConst.H_FILEONSPX, diveHeader.fileNameOnSpx );
+    values.put( ProjectConst.H_LOWTEMP, diveHeader.lowestTemp );
+    values.put( ProjectConst.H_MAXDEPTH, diveHeader.maxDepth );
+    values.put( ProjectConst.H_SAMPLES, diveHeader.countSamples );
+    values.put( ProjectConst.H_UNITS, diveHeader.units );
+    values.put( ProjectConst.H_GEO_LON, diveHeader.longgitude );
+    values.put( ProjectConst.H_GEO_LAT, diveHeader.latitude );
+    return( -1 < dBase.insertOrThrow( ProjectConst.A_TABLE_ALIASES, null, values ) );
   }
 }
