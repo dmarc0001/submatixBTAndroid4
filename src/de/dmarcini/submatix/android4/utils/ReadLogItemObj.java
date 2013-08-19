20c35d516bc3990f71decdc707c43052efc21560
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-08-19 21:02:55 +0200

    Zeit in Milisekunden als Start des TG eingefügt

--- src/de/dmarcini/submatix/android4/utils/ReadLogItemObj.java
+++ src/de/dmarcini/submatix/android4/utils/ReadLogItemObj.java
@@ -8,6 +8,7 @@ public class ReadLogItemObj
   public String  itemDetail;
   public int     dbId;
   public int     numberOnSPX;
+  public long    startTimeMilis;
   public boolean isMarked = false;
 
   /**
@@ -32,6 +33,7 @@ public class ReadLogItemObj
     this.itemDetail = _itemDetail;
     this.dbId = -1;
     this.numberOnSPX = -1;
+    this.startTimeMilis = 0L;
   }
 
   /**
@@ -57,6 +59,7 @@ public class ReadLogItemObj
     this.itemDetail = _itemDetail;
     this.dbId = _dbid;
     this.numberOnSPX = -1;
+    this.startTimeMilis = 0L;
   }
 
   /**
@@ -83,6 +86,35 @@ public class ReadLogItemObj
     this.itemDetail = _itemDetail;
     this.dbId = _dbid;
     this.numberOnSPX = _numOnSpx;
+    this.startTimeMilis = 0L;
+  }
+
+  /**
+   * 
+   * Konstruktor mit noch mehr Parametern
+   * 
+   * Project: SubmatixBTLoggerAndroid_4 Package: de.dmarcini.submatix.android4.utils
+   * 
+   * @author Dirk Marciniak (dirk_marciniak@arcor.de)
+   * 
+   *         Stand: 19.08.2013
+   * @param _isSaved
+   * @param _itemName
+   * @param _itemNameOnSpx
+   * @param _itemDetail
+   * @param _dbid
+   * @param _numOnSpx
+   * @param _milis
+   */
+  public ReadLogItemObj( final boolean _isSaved, final String _itemName, final String _itemNameOnSpx, final String _itemDetail, final int _dbid, final int _numOnSpx, long _milis )
+  {
+    this.isSaved = _isSaved;
+    this.itemName = _itemName;
+    this.itemNameOnSPX = _itemNameOnSpx;
+    this.itemDetail = _itemDetail;
+    this.dbId = _dbid;
+    this.numberOnSPX = _numOnSpx;
+    this.startTimeMilis = _milis;
   }
 
   @SuppressWarnings( "unused" )
