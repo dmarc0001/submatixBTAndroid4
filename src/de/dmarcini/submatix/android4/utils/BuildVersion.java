7edade68b5b3b77e025d609a5fe768e36440330a
Dirk Marciniak <dirk_marciniak@arcor.de>
2013-08-22 10:53:04 +0200

    Einstellung für Zeitformat zugefügt

--- src/de/dmarcini/submatix/android4/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/utils/BuildVersion.java
@@ -15,8 +15,8 @@ import java.util.Date;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 2601L;
-  private static final long buildDate = 1376945676548L;
+  private static final long buildNumber = 2607L;
+  private static final long buildDate = 1377161347062L;
   private static final String buildNumberString = String.format( "%d", buildNumber );
   private static final String buildDateString   = new Date( buildDate ).toString();
 
