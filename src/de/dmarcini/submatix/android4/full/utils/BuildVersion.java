3fbb35fa79f8e925de9854a08e93276d4558a725
Dirk Marciniak <dirk_marciniak@arcor.de>
2014-01-08 11:32:32 +0100

    BETA 09, Shop-Version 2

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -24,8 +24,8 @@ import de.dmarcini.submatix.android4.full.gui.FragmentCommonActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3066L;
-  private static final long buildDate = 1389175548181L;
+  private static final long buildNumber = 3070L;
+  private static final long buildDate = 1389176513951L;
   @SuppressWarnings( "javadoc" )
   public static final boolean isLightVersion = false;
   /**
