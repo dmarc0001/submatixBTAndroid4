88ff705199697169b578dcfd4383de6ce13267b6
Dirk Marciniak <dirk_marciniak@arcor.de>
2014-01-10 20:41:51 +0100

    Versionserhöhung für PlayStore

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -24,8 +24,8 @@ import de.dmarcini.submatix.android4.full.gui.FragmentCommonActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3130L;
-  private static final long buildDate = 1389381695729L;
+  private static final long buildNumber = 3132L;
+  private static final long buildDate = 1389382063556L;
   @SuppressWarnings( "javadoc" )
   public static final boolean isLightVersion = false;
   /**
