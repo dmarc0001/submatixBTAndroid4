c93dd9408f8f4b6e6b0f039bb1ecf4ee01f3dec8
Dirk Marciniak <dirk_marciniak@arcor.de>
2014-01-20 20:02:56 +0100

    automatisch erzeugt

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -24,8 +24,8 @@ import de.dmarcini.submatix.android4.full.gui.FragmentCommonActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3180L;
-  private static final long buildDate = 1390238851477L;
+  private static final long buildNumber = 3188L;
+  private static final long buildDate = 1390244220780L;
   @SuppressWarnings( "javadoc" )
   public static final boolean isLightVersion = false;
   /**
