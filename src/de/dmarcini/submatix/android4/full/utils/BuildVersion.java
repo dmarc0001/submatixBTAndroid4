5165fe985f2199d85422b8d28e8558669c3b1f4e
Dirk Marciniak <dirk_marciniak@arcor.de>
2014-01-22 00:23:28 +0100

    Versionserhöhung für Store

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -24,8 +24,8 @@ import de.dmarcini.submatix.android4.full.gui.FragmentCommonActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3235L;
-  private static final long buildDate = 1390345737748L;
+  private static final long buildNumber = 3237L;
+  private static final long buildDate = 1390346197601L;
   @SuppressWarnings( "javadoc" )
   public static final boolean isLightVersion = false;
   /**
