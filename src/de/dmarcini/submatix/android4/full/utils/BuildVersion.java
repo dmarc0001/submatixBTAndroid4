3a15567bb5078ab587d3048eb58ea8d390b1c270
Dirk Marciniak <dirk_marciniak@arcor.de>
2014-03-19 19:36:41 +0100

    Version 1.01 BUGFIX

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -24,8 +24,8 @@ import de.dmarcini.submatix.android4.full.gui.FragmentCommonActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3377L;
-  private static final long buildDate = 1395252547769L;
+  private static final long buildNumber = 3382L;
+  private static final long buildDate = 1395253998239L;
   /**
    * Kennzeichnung für eine Version
    */
