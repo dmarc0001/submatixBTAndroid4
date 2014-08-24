ae82c113b53ed551870060c63d902591f9dbeb4f
Dirk Marciniak <dirk_marciniak@arcor.de>
2014-08-24 13:32:48 +0200

    GPL zugefügt

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -1,4 +1,24 @@
-﻿/**
+﻿//@formatter:off
+/*
+    programm: SubmatixBTLoggerAndroid
+    purpose:  configuration and read logs from SUBMATIX SPX42 divecomputer via Bluethooth    
+    Copyright (C) 2012  Dirk Marciniak
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/
+*/
+//@formatter:on
+/**
  * Klasse beinhaltet die Buildnummer und das Erstellungsdatum
  * 
  * BuildVersion.java de.dmarcini.netutils.dsl cmdLineSequenzialDslChecker
@@ -24,8 +44,8 @@ import de.dmarcini.submatix.android4.full.gui.FragmentCommonActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3382L;
-  private static final long buildDate = 1395253998239L;
+  private static final long   buildNumber       = 3382L;
+  private static final long   buildDate         = 1395253998239L;
   /**
    * Kennzeichnung für eine Version
    */
