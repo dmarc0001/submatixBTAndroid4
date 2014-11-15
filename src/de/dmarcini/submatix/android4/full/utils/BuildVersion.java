6678c8d5624baad34b28f7591740b99e41391c01
Dirk Marciniak <dirk_marciniak@online.de>
2014-11-15 12:19:04 +0100

    automatisches Build

--- src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -44,8 +44,8 @@ import de.dmarcini.submatix.android4.full.gui.MainActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3534L;
-  private static final long buildDate = 1416046863840L;
+  private static final long buildNumber = 3543L;
+  private static final long buildDate = 1416050238963L;
   /**
    * Kennzeichnung für eine Version
    */
