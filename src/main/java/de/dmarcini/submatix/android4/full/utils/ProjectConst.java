c6d8f53d12b9b629ece210fdac525b50f32d675a
Dirk Marciniak <dirk_marciniak@online.de>
2015-03-12 21:02:18 +0100

    FErtigstellung Hilfe englisch, Version 2.0

--- src/main/java/de/dmarcini/submatix/android4/full/utils/ProjectConst.java
+++ src/main/java/de/dmarcini/submatix/android4/full/utils/ProjectConst.java
@@ -50,11 +50,11 @@ public final class ProjectConst
   public static final String MANUFACTNAME = "Dirk Marciniak";
   public static final String MANUFACTMAIL = "dirk@submatix.com";
   public static final String MANUFACTHOME = "http://www.submatix.com";
-  public static final String MANUFACTVERS = "2.0 RC1";
-  public static final String GENYEAR = "2014";
-  public static final String GENMONTH = "12";
-  public static final String GENDAY = "27";
-  public static final boolean CHECK_PHYSICAL_BT = false;
+  public static final String MANUFACTVERS = "2.0";
+  public static final String GENYEAR = "2015";
+  public static final String GENMONTH = "03";
+  public static final String GENDAY = "12";
+  public static final boolean CHECK_PHYSICAL_BT = true;
   // ANDROID: Unique UUID für allgemeine (well known) Serielle Schnittstelle
   public static final UUID SERIAL_DEVICE_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
   // Android: gewünschte BT Geräteklasse
