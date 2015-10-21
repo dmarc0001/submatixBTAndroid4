5dce7a7e1f7350ef31a9508802fd1b8a98c3cd96
Dirk Marciniak <dirk_marciniak@online.de>
2015-10-21 19:47:51 +0200

    automatische Versionierungautomatische Änderungen

--- src/main/java/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/main/java/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -43,12 +43,12 @@ import de.dmarcini.submatix.android4.full.gui.MainActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 3959L;
+  private static final long buildNumber = 3963L;
   /**
    * Kennzeichnung für eine Version
    */
   private static final String buildNumberString = String.format(Locale.ENGLISH, "%d", buildNumber);
-  private static final long buildDate = 1445447337405L;
+  private static final long buildDate = 1445449458418L;
 
   /**
    * Gib die Buildnummer zurück
