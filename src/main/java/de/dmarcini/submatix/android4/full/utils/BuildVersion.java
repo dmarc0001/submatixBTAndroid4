bfee51e6ca6b8694b3ae49fbf7a8576ece726eda
Dirk Marciniak <dirk@submatix.com>
2016-09-27 21:37:02 +0200

    Wieder Debugging eingestellt

--- src/main/java/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
+++ src/main/java/de/dmarcini/submatix/android4/full/utils/BuildVersion.java
@@ -43,12 +43,12 @@ import de.dmarcini.submatix.android4.full.gui.MainActivity;
  */
 public class BuildVersion
 {
-  private static final long buildNumber = 4087L;
+  private static final long buildNumber = 4088L;
   /**
    * Kennzeichnung für eine Version
    */
   private static final String buildNumberString = String.format(Locale.ENGLISH, "%d", buildNumber);
-  private static final long buildDate = 1475004575877L;
+  private static final long buildDate = 1475004999463L;
 
   /**
    * Gib die Buildnummer zurück
